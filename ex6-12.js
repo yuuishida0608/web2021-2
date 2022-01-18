76ff65d5defdeaeaa8a02db7d52c03c40b41f733
yuuishida0608 <zm19254@josai.ac.jp>
2022-01-18 01:54:12 +0000

    1/18

---
+++ ex6-12.js
@@ -0,0 +1,15 @@
+function hyoji1(){
+    //表示する文字
+    var str= "一文字ずつ表示します。";
+    //テキストボックスの文字数
+    var cnt=document.timer1.moji1.value.length;
+    //文字が全部表示されているか確認
+    if(cnt<11){
+    //現在より1文字多く切り出して表示
+    document.timer1.moji1.value=str.substr(0,cnt+1);}
+    //全て表示されたら、空文字に戻す
+    else{
+    document.timer1.moji1.value="";}
+    //setTimeout()を含む関数を呼び出す
+    setTimeout("hyoji1()",1000);
+}
---
+++ ex6-13.js
@@ -0,0 +1,2 @@
+function timechange(){document.getElementById('timechg').src='./image/clipborad_man.png';}
+function returnimg(){document.getElementById('timechg').src='./image/keiei_small.jpg';}
